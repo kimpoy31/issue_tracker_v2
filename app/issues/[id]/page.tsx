170f94e765b54a869b458fb5fe9de6a83c9f145e
Kim Harold <pinanonangkim000@gmail.com>
2023-12-20 15:16:06 +0800

    Refactor: Remove CSS inconcistencies

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import prisma from '@/prisma/client'
-import { Flex, Text } from '@radix-ui/themes'
+import { Box, Button, Flex, Grid, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
 import Markdown from 'react-markdown'
@@ -15,16 +15,22 @@ const DetailsPage = async({ params }:{ params: { id: string } }) => {
     if(!issue) notFound()
 
   return (
-    <div className='w-full max-w-2xl flex flex-col gap-2'>
-        <Text size="5" trim={'both'} style={{fontWeight:"bold"}}>{issue.title}</Text>
-        <div className='flex align-middle gap-2'>
-            <StatusBadge status={issue.status} />
-            <small>{issue.createdAt.toDateString()}</small>
-        </div>
-        <Markdown className="prose border p-4">
-            {issue.description}
-        </Markdown>
-    </div>
+    <Grid columns="2" gap="3">
+        <Box>
+            <Text size="5" trim={'both'} style={{fontWeight:"bold"}}>{issue.title}</Text>
+            <Flex gap={"2"}>
+                <StatusBadge status={issue.status} />
+                <small>{issue.createdAt.toDateString()}</small>
+            </Flex>
+            <Markdown className="prose border p-4">
+                {issue.description}
+            </Markdown>
+        </Box>
+        <Box>
+            <Button>Edit</Button>
+            <Button>Delete</Button>
+        </Box>
+    </Grid>
   )
 }
 
