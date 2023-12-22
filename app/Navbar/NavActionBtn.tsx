ddb30108bae4c464cbb889c68a4a75d289c113c0
Kim Harold <pinanonangkim000@gmail.com>
2023-12-22 12:04:15 +0800

    Refactor login/logout buttons

--- app/Navbar/NavActionBtn.tsx
+++ app/Navbar/NavActionBtn.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Avatar, Button, DropdownMenu, Text } from '@radix-ui/themes'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
@@ -9,7 +10,26 @@ const NavActionBtn = () => {
 
   return (
     <div>
-        { status === "authenticated" && <Link href={"/api/auth/signout"}>Logout</Link>}
+        { status === "authenticated" && 
+          // <Link href={"/api/auth/signout"}>Logout</Link>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Avatar src={session.user!.image!} fallback="?" size="2" radius='full'/>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content size="1">
+              <DropdownMenu.Label>
+                <Text>{session.user!.email}</Text>
+              </DropdownMenu.Label>
+              <DropdownMenu.Separator />
+              <Button variant="soft" size="1">
+                <Link href={"/api/auth/signout"}>Logout</Link>
+              </Button>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        }
+
+
+
         { status === "unauthenticated" && <Link href={"/api/auth/signin"}>Login</Link>}
     </div>
   )
